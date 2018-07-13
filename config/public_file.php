a43486baf067629d194779a7a73c138415fc0090
lmxdawn@qq.com <lmxdawn@qq.com>
2018-07-14 02:04:31 +0800

    增加公共文件配置

--- config/public_file.php
+++ config/public_file.php
@@ -1,12 +1,12 @@
 <?php
 
 // +----------------------------------------------------------------------
-// | 公共文件的路径配置 (注意: 最后不能加 / )
+// | 公共文件的路径配置
 // +----------------------------------------------------------------------
 
 return [
     // 上传文件的基础地址
-    'upload_base_url'   => 'http://localhost/vue-admin-php/public/uploads',
+    'upload_base_url'   => 'http://localhost/vue-admin-php/public/uploads/',
     // 静态文件的基础地址
-    'static_base_url'   => 'http://localhost/vue-admin-php/public/static',
+    'static_base_url'   => 'http://localhost/vue-admin-php/public/static/',
 ];
